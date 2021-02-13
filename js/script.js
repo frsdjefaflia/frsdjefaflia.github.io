e26afc1e44d6117d59fc5dbf89cd260818670e09
frsdjefaflia <djefaflia.fares@gmail.com>
2021-02-13 14:51:29 +0100

    add icon for close

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 var navbar = document.getElementById("navbar");
 navbar.onclick = function(){
     if (document.getElementById("list").style.left === "" || document.getElementById("list").style.left === "-100%"){
-        document.getElementById("list").style.left = "0"
+        document.getElementById("list").style.left = "0";
+        document.getElementById("open").style.display = "none";
+        document.getElementById("close").style.display = "block";
     }
     else {
         document.getElementById("list").style.left = "-100%"
+        document.getElementById("open").style.display = "block";
+        document.getElementById("close").style.display = "none";
     }
 };
